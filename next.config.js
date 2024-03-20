dc2f91a03629eae38567b1e61eb15ee3ace0d43e
Gabriel Fernandes de Oliveira <gfoliveira4@ihm.local>
2024-03-20 09:55:59 -0300

    Req Card Clubes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['media.api-sports.io'],
+      },
+}
 
 module.exports = nextConfig
--- src/app/components/Cards/posCard.js
+++ src/app/components/Cards/posCard.js
@@ -1,72 +1,88 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
 export default function PosCard() {
-    const [randomNumber, setRandomNumber] = useState(null) 
+    const [randomNumber, setRandomNumber] = useState(null)
     const [arrayClub, setArrayClub] = useState(null)
-    
- 
 
-    
-    
-    useEffect(() => {
-        function escolherNumeroAleatorio() {
-            const numeros = [33, 40, 42, 47, 49, 50, 529, 530, 541, 496, 505, 489, 85, 212, 211, 228];
-            const indiceAleatorio = Math.floor(Math.random() * numeros.length);
-            return numeros[indiceAleatorio];
-        }
+    //Função que gera um número aleatório de um array e o retorna.
+    function escolherNumeroAleatorio() {
+        const numeros = [33, 40, 42, 47, 49, 50, 529, 530, 541, 496, 505, 489, 85, 212, 211, 228];
+        const indiceAleatorio = Math.floor(Math.random() * numeros.length);
+        setRandomNumber(numeros[indiceAleatorio]);
 
-        const numeroAleatorio = escolherNumeroAleatorio();
-        setRandomNumber(numeroAleatorio);
+    }
 
-        async function getMatch(numero) {
-            const options = {
-                method: 'GET',
-                url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
-                params: {
-                    last: 1,
-                    team: `${numero}`,
-                    season: '2023'
-                },
-                headers: {
-                    'X-RapidAPI-Key': '56055e130dmshe17a666f9a8c569p1ac1f4jsn69aa95a23715',
-                    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-                }
-            };
+    //Variavel que armazena o retorno da função aleatória
 
-            try {
-                const response = await axios.request(options);
-                console.log(response.data);
-                setArrayClub(response.data);
-            } catch (error) {
-                console.error(error);
+    //Requisição para API
+    async function getMatch(numero) {
+        const options = {
+            method: 'GET',
+            url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
+            params: {
+                last: 1,
+                team: `${numero}`,
+                season: '2023'
+            },
+            headers: {
+                'X-RapidAPI-Key': '56055e130dmshe17a666f9a8c569p1ac1f4jsn69aa95a23715',
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
             }
+        };
+
+        try {
+            const response = await axios.request(options);
+            setArrayClub(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+
+
+
+/*     useEffect(() => {
+
+        if (!randomNumber) {
+            escolherNumeroAleatorio();
         }
 
-        if (randomNumber !== null) {
+        if (!arrayClub && randomNumber) {
             getMatch(randomNumber);
-            console.log(arrayClub, 'essa e avriael')
         }
-    }, [randomNumber]);
+
+
+    }, [randomNumber]); */
 
     return (
-        <div className=" bg-white h-32 w-72 rounded-xl shadow-lg flex space-x-6 justify-center items-center">
+        <>
+        {/* Criar um mini loading aqui para quando não tiver informação carregada ainda */}
+            {arrayClub && (
+                <div className=" bg-white h-32 w-72 rounded-xl shadow-lg flex space-x-6 justify-center items-center">
 
-            <div className=" flex flex-col items-center justify-center space-y-1">
-                <div className=" bg-bege h-16 w-16 rounded-full"></div>
-                <h1>
-                    ARSENAL
-                </h1>
-            </div>
-            <div className="mb-4 flex-col flex  items-center justify-center space-y-2">
-                <h3 className="text-md text-gray_strong">Finished</h3>
-                <h1 className="text-xl"><span className="pr-1">3</span>X<span className="pl-1">1</span></h1>
-            </div>
-            <div className="mt-4 flex flex-col items-center justify-center space-y-1">
-                <div className="bg-bege h-16 w-16 rounded-full"></div>
-                <h1>
-                    Everton
-                </h1>
-            </div>
-        </div>
+                    <div className="mt-4 flex flex-col items-center justify-center space-y-1">
+                        <div className=" bg-bege h-16 w-16 rounded-full">
+                            <Image src={arrayClub.response[0].teams.home.logo} height={64} width={64} alt='logoHome'></Image>
+                        </div>
+                            <h1>{arrayClub.response[0].teams.home.name}</h1>
+                    </div>
+                    <div className="mb-4 flex-col flex  items-center justify-center space-y-2">
+                        <h3 className="text-md text-gray_strong">{arrayClub.response[0].fixture.status.long}</h3>
+                        <h1 className="text-xl"><span className="pr-1">{arrayClub.response[0].goals.home}</span>X<span className="pl-1">{arrayClub.response[0].goals.away}</span></h1>
+                    </div>
+                    <div className="mt-4 flex flex-col items-center justify-center space-y-1">
+                        <div className="bg-bege h-16 w-16 rounded-full">
+                        <Image src={arrayClub.response[0].teams.away.logo} height={64} width={64} alt='logoAway'></Image>
+                        </div>
+                            <h1>{arrayClub.response[0].teams.away.name}</h1>
+                    </div>
+                </div>
+            )}
+
+        </>
     )
 }
