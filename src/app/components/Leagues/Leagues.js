0305d18aa3ef75e3b2e7b3a5bd0d80175a9e15e7
Gabriel Fernandes de Oliveira <gfoliveira4@ihm.local>
2024-03-22 00:49:24 -0300

    Req Leagues

--- src/app/components/Leagues/Leagues.js
+++ src/app/components/Leagues/Leagues.js
@@ -3,17 +3,18 @@ import MiniLeague from "../MiniCards/MiniLeague";
 import axios from 'axios';
 
 export default function Leagues() {
-    
-    const [arrayLeague, setArrayLeague] = useState([]);
+
+    const [arrayLeague, setArrayLeague] = useState(null);
     const [callApi, setCallApi] = useState(false);
 
     // Requisição para API
-    async function getLeague(numero) {
+    async function getLeague() {
+        const leagues = '39-78-135-140-61-13-2-3-94'
         const options = {
             method: 'GET',
             url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
             params: {
-                ids: '39-78-135-140-61-13-2-3-94'
+                ids: `${leagues}`
             },
             headers: {
                 'X-RapidAPI-Key': '56055e130dmshe17a666f9a8c569p1ac1f4jsn69aa95a23715',
@@ -23,32 +24,38 @@ export default function Leagues() {
 
         try {
             const response = await axios.request(options);
+            //Converte obj em array para ser mapeado no jsx
+            const array = await Object.values(response.data.response)
             // Armazena a resposta da API na variável arrayLeague
             setCallApi(true);
-            setArrayLeague(response.data);
-            console.log("Dados da API:", response.data); // Adiciona um log para depurar
+            setArrayLeague(array);
         } catch (error) {
             console.error(error);
         }
     }
 
     useEffect(() => {
-        if (!callApi){
+        if (!arrayLeague) {
             getLeague();
         }
-    }, [callApi]);
+    }, [arrayLeague]);
 
-  
-    console.log("Array League:", arrayLeague); // Adiciona um log para depurar
+
+    console.log("Array League:", arrayLeague)
 
     return (
         <div className="w-full bg-branco h-96 rounded-xl shadow-lg">
             <div className="bg-bege h-10 w-full rounded-t-xl justify-center flex items-center">
                 <h1 className="text-white">LEAGUES</h1>
             </div>
-            {arrayLeague && arrayLeague.forEach((league, index) => (
-                <MiniLeague key={league.league.id} nomeLiga={league.league.name} imgURL={league.league.logo} />
-            ))}
+
+            {arrayLeague !== null && (
+                <>
+                    {arrayLeague.map((leagueData, index) => (
+                        <MiniLeague key={index} nomeLiga={leagueData.league.name} imgURL={leagueData.league.logo} />
+                    ))}
+                </>
+            )}
         </div>
     )
 }
